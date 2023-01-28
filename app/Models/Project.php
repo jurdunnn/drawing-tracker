b946c737047737270580de04e20160fb815dfad0
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-01-28 20:36:50 +0000

    Add project relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    public function color()
+    {
+        return $this->hasOne(Color::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function drawings()
+    {
+        return $this->hasMany(Drawing::class);
+    }
 }
