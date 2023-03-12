cf654b9365f7d4062c39ac7c0b8b83a1f33f4d2b
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-03-12 12:04:10 +0000

    Fix project button styling at different breakpoints

--- resources/views/livewire/projects.blade.php
+++ resources/views/livewire/projects.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-full p-1 text-gray-400 md:p-32 gap-y-8" x-show="fullscreen == 'false'" x-cloak>
+<div class="flex flex-col w-full p-1 px-0 text-gray-400 sm:py-32 sm:px-24 md:px-2 gap-y-8" x-show="fullscreen == 'false'" x-cloak>
     <div>
         <h1 class="text-4xl font-bold text-white">Hi {{ Auth::user()->name ?? ''  }}</h1>
         <h3>Welcome back to the workspace, we missed you!</h3>
@@ -17,7 +17,7 @@
             />
     </div>
 
-    <div class="mt-12 grid grid-cols-3 gap-x-1 md:gap-x-0 gap-y-12">
+    <div class="mt-12 grid grid-cols-2 sm:grid-cols-3 gap-x-1 md:gap-x-0 gap-y-12">
         @foreach($projects as $project)
             <a wire:click="redirectToProject({{ $project->id }})" class="flex flex-col cursor-pointer max-w-content gap-y-2 hover:scale-105 slow-hover">
                 <div class="relative flex items-center justify-center ml-auto mr-auto text-3xl font-bold text-white bg-{{ $project->color->name }}-500 w-28 h-28 rounded-2xl">
