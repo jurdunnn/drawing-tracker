dd6af44691f662e88ca0e1170fab3710d1f55064
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-03-15 22:19:21 +0000

    Revert "Unistall flowbite"
    This reverts commit 7ef8c2f82c0d95866f460ee900dda3a1c6cc0ede.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js",
   ],
   safelist: [
     {
@@ -14,4 +15,7 @@ module.exports = {
   theme: {
     extend: {},
   },
+  plugins: [
+      require('flowbite/plugin'),
+  ],
 }
