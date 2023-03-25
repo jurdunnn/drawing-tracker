0e6ddeba1c4f625e394be8b57ade770f113000c4
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-03-25 11:04:56 +0000

    Reintroduce wire poll on view wrapper

--- resources/views/components/view-wrapper.blade.php
+++ resources/views/components/view-wrapper.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gray-100 rounded-lg shadow-2xl">
+<div class="bg-gray-100 rounded-lg shadow-2xl" wire:poll.500ms>
     <div class="relative flex flex-col h-full text-gray-500">
         {{ $slot }}
     </div>
